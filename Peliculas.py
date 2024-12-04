36255d4b3fd29a003d980326749b70b17079fed7
Ruben Barrionuevo <101405484+RubenAgustinB@users.noreply.github.com>
2024-12-04 02:22:53 -0300

    Delete Peliculas.py

--- Peliculas.py
+++
@@ -1,8 +0,0 @@
-class Pelicula:
-
-    def __init__(self,nombre,genero,popularidad,duracion):
-        self.nombre=nombre
-        self.genero=genero
-        self.duracion=duracion
-        self.popularidad=popularidad
-#coment
